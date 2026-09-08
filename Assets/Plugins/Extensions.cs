0c33e41965570393273e43ed3025b82ba85903dd
louve <louve@systems>
2026-09-08 16:30:35 +0200

    TMPro

---
+++ Assets/Plugins/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEditor;
+
+public static class PluginExtensions
+{
+#if UNITY_EDITOR
+    public static System.Type AssetDatabase_GetMainAssetTypeAtPath(string assetPath)
+    {
+        var loaded = AssetDatabase.LoadMainAssetAtPath(assetPath);
+        System.Type type = loaded.GetType();
+        UnityEngine.Object.Destroy(loaded);
+        return type;
+    }
+#endif
+}
